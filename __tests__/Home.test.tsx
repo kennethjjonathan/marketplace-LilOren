5af03fbcd0073a568ddf80e6d038ba12b7c5e1e7
kenneth.jonathan <kenneth.jonathan@shopee.com>
2023-10-30 19:00:50 +0700

    feat(project-setup):initialized next js app and set up jest

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/pages/index";
+
+describe("Home", () => {
+  it("should have Docs text", () => {
+    render(<Home />); //ARRANGE
+
+    const myElem = screen.getByText("Docs"); //ACT
+    expect(myElem).toBeInTheDocument(); //ASSERT
+  });
+
+  it("should contain the 'information' text", () => {
+    render(<Home />); //ARRANGE
+
+    const myElem = screen.getByText(/information/i); //ACT
+    expect(myElem).toBeInTheDocument(); //ASSERT
+  });
+
+  it("should have a heading", () => {
+    render(<Home />); //ARRANGE
+
+    const myElem = screen.getByRole("heading", {
+      name: "Learn",
+    }); //ACT
+    expect(myElem).toBeInTheDocument(); //ASSERT
+  });
+});
