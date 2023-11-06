b854139113675279b059595a305a32793854d9ca
kenneth.jonathan <kenneth.jonathan@shopee.com>
2023-11-06 10:51:12 +0700

    refactor(product-page): refactor the code for quantity controller

--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -11,32 +11,24 @@ function QuantityController({
   inputValue,
   setInputValue,
   maximum,
-  ...props
 }: QuantityControllerProps) {
   return (
-    <div
-      className="p-1 bg-input flex items-center gap-4 w-fit lg:gap-5"
-      {...props}
-    >
-      {inputValue === 1 ? (
-        <button disabled>
-          <Minus className="text-[#777777] w-5 aspect-square lg:w-6" />
-        </button>
-      ) : (
-        <button onClick={() => setInputValue((prev) => prev - 1)}>
-          <Minus className="text-primary w-5 aspect-square lg:w-6" />
-        </button>
-      )}
+    <div className="p-1 bg-input flex items-center gap-4 w-fit lg:gap-5">
+      <button
+        onClick={() => setInputValue((prev) => prev - 1)}
+        disabled={inputValue === 1}
+        className="text-primary w-5 aspect-square lg:w-6 disabled:text-[#777777] disabled:cursor-not-allowed"
+      >
+        <Minus />
+      </button>
       <p className="text-base font-semibold lg:text-lg">{inputValue}</p>
-      {inputValue === maximum ? (
-        <button disabled>
-          <Plus className="text-[#777777] w-5 aspect-square lg:w-6" />
-        </button>
-      ) : (
-        <button onClick={() => setInputValue((prev) => prev + 1)}>
-          <Plus className="text-primary w-5 aspect-square lg:w-6" />
-        </button>
-      )}
+      <button
+        onClick={() => setInputValue((prev) => prev + 1)}
+        disabled={inputValue === maximum}
+        className="text-primary w-5 aspect-square lg:w-6 disabled:text-[#777777] disabled:cursor-not-allowed"
+      >
+        <Plus />
+      </button>
     </div>
   );
 }
