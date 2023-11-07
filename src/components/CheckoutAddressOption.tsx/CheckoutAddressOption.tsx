d2096b697cf0e0f62204f8300fe074fd18649438
kenneth.jonathan <kenneth.jonathan@shopee.com>
2023-11-07 16:40:19 +0700

    feat(checkout page): created the overall ui for chcekout page

--- src/components/CheckoutAddressOption.tsx/CheckoutAddressOption.tsx
+++ src/components/CheckoutAddressOption.tsx/CheckoutAddressOption.tsx
@@ -3,23 +3,21 @@ import { MapPin, ChevronRight } from 'lucide-react';
 
 const CheckoutAddressOption = () => {
   return (
-    <div className="w-full rounded-md shadow-xl border-[1px]">
+    <div className="w-full rounded-md shadow-md border-[1px]">
       <div className="flex items-center gap-2 p-2 border-b-[1px] border-gray-200 w-full">
-        <MapPin className="text-primary" size={30} />
-        <p className="text-base">Your Shipping Address</p>
+        <MapPin className="text-primary w-6 h-6 sm:w-7 sm:h-7" />
+        <p className="sm:text-lg">Your Shipping Address</p>
       </div>
       <div className="w-full p-2 flex justify-between items-center">
         <div className="w-10/12">
-          <p className="font-semibold text-lg leading-tight">
+          <p className="font-semibold text-lg leading-tight sm:text-xl">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           </p>
-          <p className="text-sm text-gray-500">
+          <p className="text-sm text-gray-500 sm:text-base">
             Kenneth J. Jonathan, 081298562868
           </p>
         </div>
-        <div className="w-1/12">
-          <ChevronRight className="h-8 w-8" />
-        </div>
+        <ChevronRight className="h-8 w-8 sm:w-9 sm:h-9" />
       </div>
     </div>
   );
---
+++ src/components/CheckoutProductListTab/CheckoutProductListTab.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { IProduct } from '@/interface/product';
+import Image from 'next/image';
+import moneyFornatter from '@/lib/moneyFornatter';
+
+interface CheckoutProductListTabProps {
+  item: IProduct;
+}
+
+const CheckoutProductListTab = ({ item }: CheckoutProductListTabProps) => {
+  return (
+    <div className="w-full py-2">
+      <p className="font-semibold sm:text-lg">Shop 1</p>
+      <div className="flex mt-1 items-start gap-2">
+        <div className="relative aspect-square rounded-md overflow-hidden h-24 sm:h-28">
+          <Image
+            src={item.image}
+            alt={`${item.name}'s photo`}
+            fill
+            style={{ objectFit: 'cover' }}
+            sizes="(max-width: 768px) 30vw, 20vw"
+          />
+        </div>
+        <div className="flex flex-col flex-1 gap-1">
+          <p className="text-gray-500 text-sm leading-tight sm:text-base">
+            {item.name}
+          </p>
+          <p className="text-gray-500 text-xs leading-none sm:text-sm">{`Variant: ${item.variant}`}</p>
+          <p className="font-semibold text-base sm:text-lg">{`${
+            item.quantity
+          } x ${moneyFornatter(item.price)}`}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutProductListTab;
