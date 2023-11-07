2be2356a50410306885f5eb4ed1d5b25178f79cd
Endriyani Rahayu <endriyani.rahayu@shopee.com>
2023-11-07 11:14:16 +0700

    feat(configure-delivery-seller): added seller layout with navbar component

--- src/components/SellerLayout/Sidebar.tsx
+++ src/components/SellerLayout/Sidebar.tsx
@@ -1,26 +1,24 @@
-import React from 'react'
-import classNames from 'classnames'
+import React from 'react';
+import classNames from 'classnames';
 
 interface SidebarProps {
-  open:boolean
-  setOpen:(open:boolean)=>void
+  open: boolean;
+  setOpen: (open: boolean) => void;
 }
 
-const Sidebar = ({open, setOpen}:SidebarProps) => {
+const Sidebar = ({ open, setOpen }: SidebarProps) => {
   return (
-<div
+    <div
       className={classNames({
-        "flex flex-col justify-between": true, // layout
-        "bg-indigo-700 text-zinc-50": true, // colors
-        "md:w-full md:sticky md:top-16 md:z-0 top-0 z-20 fixed": true, // positioning
-        "md:h-[calc(100vh_-_64px)] h-full w-[300px]": true, // for height and width
-        "transition-transform .3s ease-in-out md:translate-x-0": true, //animations
-        "-translate-x-full ": !open, //hide sidebar to the left when closed
+        'flex flex-col justify-between': true, // layout
+        'bg-indigo-700 text-zinc-50': true, // colors
+        'md:w-full md:sticky md:top-16 md:z-0 top-0 z-20 fixed': true, // positioning
+        'md:h-[calc(100vh_-_64px)] h-full w-[300px]': true, // for height and width
+        'transition-transform .3s ease-in-out md:translate-x-0': true, //animations
+        '-translate-x-full ': !open, //hide sidebar to the left when closed
       })}
-    >
-      
-    </div>
-  )
-}
+    ></div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
