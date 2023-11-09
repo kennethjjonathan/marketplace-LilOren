9189672d55edd67b740e2016e667b75593a1d84f
Endriyani Rahayu <endriyani.rahayu@shopee.com>
2023-11-09 10:42:12 +0700

    feat(seller-add-new-product): added photos in seller add new product page

--- src/pages/seller/portal/product/create/index.tsx
+++ src/pages/seller/portal/product/create/index.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, ReactElement, useState } from 'react';
 import Image from 'next/image';
 import { Label } from '@radix-ui/react-label';
-import { ArrowUpFromLine, XCircle } from 'lucide-react';
+import { ArrowUpFromLine, Info, XCircle } from 'lucide-react';
 import SellerLayout from '@/components/SellerLayout/SellerLayout';
 import styles from './SellerPortalProductCreate.module.scss';
 
@@ -16,7 +16,7 @@ const SellerPortalProductCreate = () => {
   const [remainingPhotos, setRemainingPhotos] = useState<number>(6);
   const [tempProductPhotos, setTempProductPhotos] = useState<File[]>([]);
 
-  const handleAddPhoto = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleAddPhoto = (e: ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = e.target.files as FileList;
     setTempProductPhotos([...tempProductPhotos, selectedFiles?.[0]]);
     setRemainingPhotos((prev) => prev - 1);
@@ -40,15 +40,38 @@ const SellerPortalProductCreate = () => {
         >
           <p className={`${styles.section_title}`}>{'Product Information'}</p>
           <form>
-            <div className={`flex flex-col`}>
+            <div className={`flex flex-col w-fit`}>
               {/* Product Photos */}
-              <Label
-                htmlFor={'index'}
-                className="font-light w-full text-[12px] md:text-base"
-              >
-                {'Product Photo'}
-                <span className="text-primary">{' *'}</span>
-              </Label>
+              <div className="flex flex-row">
+                <Label
+                  htmlFor={'index'}
+                  className="font-light w-full text-[10px] lg:text-[12px] md:text-base flex flex-row gap-2"
+                >
+                  <>
+                    <p className="text-[12px] lg:text-[14px]">
+                      {'Product Photos'}
+                    </p>
+                    <span className="text-primary">{' *'}</span>
+                  </>
+                  <div className={`${styles.product_icon} hidden lg:block`}>
+                    <Info size={15} className="text-muted-foreground" />
+                    <div
+                      className={`${styles.product_info} bg-white sm:w-[350px] md:w-[400px] lg:w-[500px] text-[12px] absolute p-6 rounded-xl border-2 duration-500 before:ease-in-out after:ease-in-out `}
+                    >
+                      <span>
+                        Image format .jpg .jpeg .png and minimum size 300 x
+                        300px (For optimal images use a minimum size of 700 x
+                        700 px)
+                      </span>
+                      <span>
+                        {
+                          "Select a product photo. Upload min. 3 photos that are interesting and different from each other to attract buyers' attention."
+                        }
+                      </span>
+                    </div>
+                  </div>
+                </Label>
+              </div>
               <div className={`input-photos flex flex-wrap`}>
                 {tempProductPhotos.map(
                   (file, index) =>
@@ -74,18 +97,16 @@ const SellerPortalProductCreate = () => {
                 {remainingPhotos !== 0 && (
                   <div className="p-3">
                     <label
-                      className="border-2 border-dashed flex flex-col justify-center items-center h-[75px] w-[75px] gap-2 hover:border-primary duration-500 before:ease-in-out after:ease-in-out hover:text-primary hover:bg-primary/5 lg:h-[100px] lg:w-[100px] rounded-lg mr"
+                      className="border-2 border-dashed flex flex-col justify-center items-center h-[75px] w-[75px] gap-2 hover:border-primary duration-500 before:ease-in-out after:ease-in-out text-primary hover:bg-primary/5 lg:h-[100px] lg:w-[100px] rounded-lg mr"
                       htmlFor={`key-image:${tempProductPhotos.length - 1}`}
                     >
                       <ArrowUpFromLine />
-                      <p className="text-[10px] font-bold">{`${remainingPhotos} / 6`}</p>
-                      <p className="text-[10px] md:text-[12px]">Upload Photo</p>
+                      <p className="text-[10px] lg:text-[12px] font-bold">{`${remainingPhotos} / 6`}</p>
+                      <p className="text-[10px] lg:text-[12px]">Upload Photo</p>
                     </label>
                     <input
                       accept="image/png, image/jpeg, image/jpg"
-                      onChange={(e) =>
-                        handleAddPhoto(e, tempProductPhotos.length - 1)
-                      }
+                      onChange={(e) => handleAddPhoto(e)}
                       type="file"
                       id={`key-image:${tempProductPhotos.length - 1}`}
                       name="filename"
@@ -96,6 +117,7 @@ const SellerPortalProductCreate = () => {
                 )}
               </div>
             </div>
+            <div className={``}></div>
           </form>
         </section>
       </div>
