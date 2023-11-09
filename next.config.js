c9485a2117d07187b13c6eca67883ee77d27080e
kenneth.jonathan <kenneth.jonathan@shopee.com>
2023-11-09 18:10:54 +0700

    feat(signin): edit userServer for development

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASEURL: process.env.BASEURL
+  },
   images: {
     remotePatterns: [
       {
