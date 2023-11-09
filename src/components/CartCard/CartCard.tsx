4c46bde81489dbf6d35266368661532af7dff2e4
Endriyani Rahayu <endriyani.rahayu@shopee.com>
2023-11-09 11:03:16 +0700

    feat(cart-page): added divider component

--- src/components/CartCard/CartCard.tsx
+++ src/components/CartCard/CartCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IProduct } from '@/interface/product';
 import { Checkbox } from '../ui/checkbox';
 import CartCardProduct from '../CartCardProduct.tsx/CartCardProduct';
+import Divider from '../Divider/Divider';
 
 interface CartCardProps {
   shop: string;
@@ -10,7 +11,7 @@ interface CartCardProps {
 
 const CartCard = ({ shop, items }: CartCardProps) => {
   return (
-    <div className="flex flex-col w-full rounded-md shadow-xl border-[1px] border-gray-100">
+    <div className="flex flex-col w-full rounded-md border-[1px] border-gray-100">
       <div className="flex items-center gap-2 p-2 border-b-[1px] border-gray-200 w-full">
         <Checkbox
           id={`check-${shop}`}
@@ -21,8 +22,9 @@ const CartCard = ({ shop, items }: CartCardProps) => {
       <div className="w-full flex flex-col gap-2 p-2 divide-x-2">
         {items.map((items, index) => (
           <CartCardProduct key={index} product={items} />
-        ))}
+          ))}
       </div>
+          <Divider />
     </div>
   );
 };
