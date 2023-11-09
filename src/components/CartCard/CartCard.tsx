d0c44ba8f9888d60e511857d7fc1ea88c50dba7c
Endriyani Rahayu <endriyani.rahayu@shopee.com>
2023-11-09 11:44:05 +0700

    feat(cart-page): updated cart layout style

--- src/components/CartCard/CartCard.tsx
+++ src/components/CartCard/CartCard.tsx
@@ -11,13 +11,15 @@ interface CartCardProps {
 
 const CartCard = ({ shop, shop_items }: CartCardProps) => {
   return (
-    <div className="flex flex-col w-full rounded-md border-[1px] border-gray-100">
+    <div className="flex flex-col w-full border-[1px] border-gray-100">
       <div className="flex items-center gap-2 p-2 border-b-[1px] border-gray-200 w-full">
         <Checkbox
           id={`check-${shop}`}
           className="sm:w-5 sm:h-5 xl:w-6 xl:h-6"
         />
-        <p className="font-semibold text-sm md:text-base line-clamp-1 overflow-hidden whitespace-nowrap text-elipsis">{shop}</p>
+        <p className="font-semibold text-sm md:text-base line-clamp-1 overflow-hidden whitespace-nowrap text-elipsis">
+          {shop}
+        </p>
       </div>
       <div className="w-full flex flex-col gap-2 p-2 divide-x-2">
         {shop_items.map((items, index) => (
--- src/components/CartLayout/CartLayout.tsx
+++ src/components/CartLayout/CartLayout.tsx
@@ -6,36 +6,43 @@ const CartLayout = () => {
   return (
     <section className="w-full fixed bottom-0 left-0 bg-primary-foreground">
       <div className="w-full md:w-[75vw] p-2 pb-3 flex items-center justify-between mx-auto gap-2">
-        <div className="flex items-center gap-2 justify-between min-w-fit sm:gap-8">
-          <div className="flex items-center gap-2 min-w-fit">
-            <Checkbox
-              id="check-all-product"
-              className="sm:h-5 sm:w-5 xl:w-6 xl:h-6"
-            />
-            <label
-              htmlFor="check-all-product"
-              className="text-sm flex flex-col items-start sm:text-base sm:flex-row sm:gap-1 xl:text-lg"
-            >
-              <p>All</p>
-              <p>Products</p>
-            </label>
+        <div className="flex items-center gap-2 justify-between w-full sm:gap-8">
+          <div className="flex items-center gap-2 min-w-fit justify-between">
+            {/* checkbox label */}
+            <div className="flex flex-row gap-2 items-center">
+              <Checkbox
+                id="check-all-product"
+                className="sm:h-5 sm:w-5 xl:w-6 xl:h-6"
+              />
+              <label
+                htmlFor="check-all-product"
+                className="text-sm flex flex-col items-start sm:text-base sm:flex-row sm:gap-1 xl:text-lg"
+              >
+                <p className="text-[12px] md:text-[14px] lg:text-[16px] text-muted-foreground">
+                  All
+                </p>
+              </label>
+            </div>
           </div>
-          <div className="flex flex-col items-start min-w-fit">
-            <p className="text-xs text-gray-500 sm:text-sm xl:text-base">
-              Total
-            </p>
-            <p className="text-base font-semibold sm:text-xl xl:text-2xl">
-              Rp 1.900.000
-            </p>
+          {/* total checkout button */}
+          <div className="flex flex-row gap-2 lg:gap-4 items-center">
+            <div className="text-right justify-end items-start">
+              <p className="text-xs text-gray-500 sm:text-sm xl:text-base text-right">
+                Total
+              </p>
+              <p className="text-base font-semibold sm:text-xl xl:text-2xl text-right">
+                Rp 1.900.000
+              </p>
+            </div>
+            <Button
+              variant={'default'}
+              size={'customBlank'}
+              className="w-[100px] lg:w-[200px] h-full p-2 text-sm sm:text-base lg:text-lg"
+            >
+              Check Out
+            </Button>
           </div>
         </div>
-        <Button
-          variant={'tertiary'}
-          size={'customBlank'}
-          className="w-full h-full p-2 text-base sm:w-2/5 sm:text-lg xl:text-xl"
-        >
-          Check Out
-        </Button>
       </div>
     </section>
   );
