de779549bb63b673d100075e9dd98c480dcc45d2
kenneth.jonathan <kenneth.jonathan@shopee.com>
2023-11-16 20:27:38 +0700

    feat(order detail): implement pagination and status filter order page

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -14,12 +14,7 @@ interface TabsProps {
   currentTab: string;
   setCurrentTab: Dispatch<SetStateAction<string>>;
 }
-const Tabs = ({ datas, currentTab, setCurrentTab }: TabsProps) => {
-  const [selectedTab, setSelectedTab] = useState(null);
-  const handleChangeTab = (current: string) => {
-    setCurrentTab(current);
-  };
-
+const Tabs = ({ datas, currentTab }: TabsProps) => {
   return (
     <div
       className={`bg-white shadow-sm rounded-t-xl flex gap-2 p-2 overflow-x-auto`}
@@ -27,11 +22,7 @@ const Tabs = ({ datas, currentTab, setCurrentTab }: TabsProps) => {
       {datas.map((data) => (
         <div key={`key:${data.id}`}>
           <Link href={data.href}>
-            <Button
-              variant={'ghost'}
-              className="p-0 hover:text-primary"
-              onClick={() => handleChangeTab(data.status!)}
-            >
+            <Button variant={'ghost'} className="p-0 hover:text-primary">
               <p
                 className={`text-[12px] lg:text-[14px] p-4 ${
                   currentTab === data.status && 'text-primary'
@@ -44,7 +35,7 @@ const Tabs = ({ datas, currentTab, setCurrentTab }: TabsProps) => {
           {currentTab === data.status && (
             <div
               className={`border-2 border-primary rounded-xl translate-x- duration-150 ease-in-out`}
-            ></div>
+            />
           )}
         </div>
       ))}
--- src/pages/user/order-detail/index.tsx
+++ src/pages/user/order-detail/index.tsx
@@ -1,5 +1,5 @@
 import { NextPageWithLayout } from '@/pages/_app';
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useCallback, useEffect, useState } from 'react';
 import Layout from '@/components/Layout/Layout';
 import Tabs from '@/components/Tabs/Tabs';
 import Divider from '@/components/Divider/Divider';
@@ -8,6 +8,7 @@ import { IOrderItem } from '@/interface/orderDetailPage';
 import BuyerOrderDetailCard from '@/components/BuyerOrderDetailCard/BuyerOrderDetailCard';
 import PaginationNav from '@/components/PaginationNav/PaginationNav';
 import { useSearchParams } from 'next/navigation';
+import DotsLoading from '@/components/DotsLoading/DotsLoading';
 const data = [
   {
     id: 1,
@@ -55,50 +56,103 @@ const data = [
 
 const OrderDetailPage: NextPageWithLayout = () => {
   const searchParams = useSearchParams();
+  const status = searchParams.get('status');
   const [orderItems, setOrderItems] = useState<IOrderItem[]>();
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPage, setTotalPage] = useState<number>(0);
-  const [currentTab, setCurrentTab] = useState<string>('');
-  const status = searchParams.get('status');
-  const page = searchParams.get('page');
+  const [currentTab, setCurrentTab] = useState<string>(
+    status !== null ? status : '',
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  async function getItems() {
-    console.log(status, page);
+  const handleChangeStatus = useCallback(async () => {
+    setIsLoading(true);
+    status === null ? setCurrentTab('') : setCurrentTab(status);
+    console.log('test');
     try {
-      const response = await axiosInstance(`/orders`);
+      const response = await axiosInstance(
+        `/orders?${
+          status === '' || status === null
+            ? 'page=1'
+            : `page=1&status=${status}`
+        }`,
+      );
       setOrderItems(response.data.data.order);
       setTotalPage(response.data.data.pagination.total_page);
       setCurrentPage(response.data.data.pagination.current_page);
-      console.log(response);
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
     } catch (error) {
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
-  }
+  }, [status]);
+
+  const handleChangePage = useCallback(async () => {
+    setIsLoading(true);
+    console.log('halo');
+    try {
+      const response = await axiosInstance(
+        `/orders?${
+          status === '' || status === null
+            ? `page=${currentPage}`
+            : `page=${currentPage}&status=${status}`
+        }`,
+      );
+      setOrderItems(response.data.data.order);
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [currentPage]);
+
   useEffect(() => {
-    getItems();
-  }, [searchParams]);
+    handleChangePage();
+  }, [handleChangePage]);
+
+  useEffect(() => {
+    handleChangeStatus();
+  }, [handleChangeStatus]);
   return (
     <section className="w-full bg-white">
-      <div className="w-full md:w-[75vw] lg:px-2 lg:pt-5 pb-5 flex flex-col mx-auto">
-        <Tabs
-          datas={data}
-          currentTab={currentTab}
-          setCurrentTab={setCurrentTab}
-        />
-        <Divider />
-        <div className="w-full pb-2">
-          {orderItems &&
-            orderItems.map((orderItem, index) => (
-              <BuyerOrderDetailCard key={index} orderItem={orderItem} />
-            ))}
-        </div>
-        <div className="w-full flex items-center justify-center mt-3">
-          <PaginationNav
-            totalPage={totalPage}
-            currentPage={currentPage}
-            setCurrentPage={setCurrentPage}
-          />
-        </div>
+      <div
+        className={`w-full md:w-[75vw] lg:px-2 lg:pt-5 pb-5 flex flex-col mx-auto`}
+      >
+        {isLoading ? (
+          <>
+            <DotsLoading />
+          </>
+        ) : (
+          <>
+            <Tabs
+              datas={data}
+              currentTab={currentTab}
+              setCurrentTab={setCurrentTab}
+            />
+            <Divider />
+            <div className="w-full pb-2">
+              {orderItems &&
+                orderItems.map((orderItem, index) => (
+                  <BuyerOrderDetailCard key={index} orderItem={orderItem} />
+                ))}
+            </div>
+            <div className="w-full flex items-center justify-center mt-3">
+              <PaginationNav
+                totalPage={totalPage}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+              />
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
