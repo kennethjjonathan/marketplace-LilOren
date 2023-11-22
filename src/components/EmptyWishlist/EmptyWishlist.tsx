79969055e9b0c4cb68d4fa9084ad16a08c41ed78
Endriyani Rahayu <endriyani.rahayu@shopee.com>
2023-11-22 23:28:29 +0700

    feat(wishlist): updated empty wishlist component

--- src/components/EmptyWishlist/EmptyWishlist.tsx
+++ src/components/EmptyWishlist/EmptyWishlist.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import { Button } from '../ui/button';
 
 const EmptyWishlist = () => {
+  const router = useRouter();
   return (
-    <div className="h-[100vh] flex justify-center items-center">
-      Empty Wishlist
+    <div className="max-h-max flex flex-col gap-4 justify-center items-center pt-3 lg:pt-3">
+      <img
+        src="/empty-wishlist.png"
+        alt="empty-wishlist"
+        className="w-[200px]"
+      />
+      <div className="w-[250px] md:w-[400px] lg:w-[300px] flex flex-col gap-3 items-center justify-center">
+        <p className="font-bold">{'Your wishlist is still empty'}</p>
+        <p className="text-[14px] text-muted-foreground text-center">
+          {
+            'Fill it with your desired products and make your Wishlist come true!'
+          }
+        </p>
+      </div>
+      <Button onClick={() => router.push('/')}>{'Search for products'}</Button>
     </div>
   );
 };
